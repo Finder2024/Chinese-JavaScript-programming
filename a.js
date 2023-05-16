0832cbb10ed7a7297aff82be4e6345adfd49c7d1
T-lXl <133726840+T-lXl@users.noreply.github.com>
2023-05-16 10:12:41 +0800

    更新源代码

--- a.js
+++ a.js
@@ -1,11 +1,3 @@
-/*
- * @Author: lxl 118715366+lxl2021@users.noreply.github.com
- * @Date: 2023-05-11 21:28:50
- * @LastEditors: lxl 118715366+lxl2021@users.noreply.github.com
- * @LastEditTime: 2023-05-16 10:11:21
- * @FilePath: /undefined/Users/lvxuanlin/Documents/a.js
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
 const { default: axios } = require("axios");
 const e = require("express");
 
@@ -57,12 +49,3 @@ async function 发起网络请求(methods, urls, headerses,datas) {
         throw error; // 抛出错误
     }
 }
-发起网络请求('GET', 'https://v.api.aa1.cn/api/api-wenan-anwei/index.php?type=json').then(result => {
-    控制台打印(result.anwei)
-}).catch(error => {
-    控制台打印(error);
-});
-定义变量('myVar',[0,0,0,1,0,1,0,1]);
-console.log(myVar); // 输出 10
-// console.log(发起网络请求('GET', 'https://v.api.aa1.cn/api/api-wenan-anwei/index.php?type=json'));
-// 控制台打印("HHH")
